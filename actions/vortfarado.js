70da79407f0005c1ddd6e3c32dc6b5491587b4b2
Kristian Soto <kroyxlab@gmail.com>
2021-11-30 20:20:29 -0400

    Aldonu eraran kontrolobiton al dosiero vortfarado

--- actions/vortfarado.js
+++ actions/vortfarado.js
@@ -3,9 +3,18 @@ module.exports = {
 	execute(ctx) {
 		const rezulto = ctx.match.input.split("_")[1];
 		ctx.db.vortfaradaRezulto = rezulto;
+
+		if (
+			ctx.db.vortfarado.vortfarado === undefined ||
+			ctx.db.vortfarado.vortfarado[0].partoj.partoj === undefined
+		) {
+			ctx.deleteMessage();
+			return -1;
+		}
 		const vortfarado = ctx.db.vortfarado.vortfarado.filter(
 			(vortfarado) => vortfarado.rezulto === rezulto
 		);
+
 		const partoj = vortfarado[0].partoj
 			.filter((parto) => parto.vorto !== null)
 			.map((parto) => parto.vorto);
